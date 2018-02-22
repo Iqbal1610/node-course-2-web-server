a6c7446168d641407d31db886ecdf0e8437036e7
iqbal rubel <iqbalict39@outlook.com>
2018-02-22 11:24:30 +0600

    add project page

--- server.js
+++ server.js
@@ -57,6 +57,12 @@ app.get('/about',(req,res)=>{
   });//using handlebarjs
 });
 
+app.get('/projects',(req,res)=>{
+  res.render('projects.hbs',{
+    pageTitle:'Projects'
+  });
+});
+
 // /bad - send back json with errorMessage
 app.get('/bad',(req,res)=>{
   res.send({
